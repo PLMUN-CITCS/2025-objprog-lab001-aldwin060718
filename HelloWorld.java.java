06e4c4c7d088abb282dc9afe209bce08a1223d6a
aldwin060718 <tapacaldwinnorman_bsit@plmun.edu.ph>
2025-02-14 12:40:49 +0800

    My first java program

---
+++ HelloWorld.java.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+  // Ang main method ay ang entry point ng programa
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");  // Ipiniprint ang mensahe sa console
+  }
+}
